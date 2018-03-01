40b6dae982c53d48543ea6e3a5be8823e8c489c5
Fernando Frances <fernando95frances@gmail.com>
2018-03-01 10:37:07 +0100

    Initial Commit, empty app

---
+++ podium-iOS/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  podium-iOS
+//
+//  Created by Fernando Frances on 01/03/2018.
+//  Copyright © 2018 Fernando Frances. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+
+
+}
+
