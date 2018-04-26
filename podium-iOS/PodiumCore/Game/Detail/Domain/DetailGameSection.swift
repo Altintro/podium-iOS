973df0f8b46722e4fcd0e52b0e2923c19a6c8665
Fernando Frances <fernando95frances@gmail.com>
2018-04-26 11:11:49 +0200

    Added detailGameSection struct

---
+++ podium-iOS/PodiumCore/Game/Detail/Domain/DetailGameSection.swift
@@ -0,0 +1,13 @@
+//
+//  DetailGameSection.swift
+//  podium-iOS
+//
+//  Created by Fernando Frances on 25/04/2018.
+//  Copyright © 2018 Fernando Frances. All rights reserved.
+//
+
+import Foundation
+
+enum DetailGameSection {
+
+}
